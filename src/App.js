a64d0f224f1776d908b9c81c57857c6dc38ae16b
nafish08 <md.nafish.sadik@gmail.com>
2022-06-09 23:48:19 +0600

    Hero and navbar customized

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="">
       <Header></Header>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className="hero min-h-screen" style={{
-            backgroundImage: `url("https://api.lorem.space/image/fashion?w=1000&h=800")`
-        }}>
+        <div className="hero min-h-screen">
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <h1 className="mb-5 text-5xl font-bold">Hello! I'm <br /> I am Md. Nafish Sadik</h1>
+                    <p className="mb-5">Front-end/Back-end developer.</p>
+                    <button className="btn btn-primary">Download Resume</button>
                 </div>
             </div>
         </div>
