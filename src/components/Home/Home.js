54a6d2c19b7db3a0282aa089a8fe1afdb4831896
MD Nafish Sadik <md.nafish.sadik@gmail.com>
2023-12-09 10:17:12 +0600

    Commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
                 <div className="hero-overlay bg-opacity-60 "></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello! I'm <br /> I am Md. Nafish Sadik</h1>
+                        <h1 className="mb-5 text-5xl font-bold">Hello!<br /> I am MD Nafish Sadik</h1>
                         <p className="mb-5">Front-end/Back-end developer.</p>
                         <button className="btn btn-primary"><a href='https://drive.google.com/file/d/1b_q8eZSXMGgtwFvUvmcL_JcJvddfB-VX/view?usp=sharing' target='_blank' download>Download Resume</a></button>
                     </div>
